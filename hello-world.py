05e6f6e702547d46648b7a694d3d91909bc12c68
Daniel Wärnelöv <daniel.pean@gmail.com>
2023-06-19 12:32:13 +0200

    test again

--- hello-world.py
+++ hello-world.py
@@ -1,17 +1,5 @@
-#!/usr/bin/env python3
-
-"""
-    my application.
-"""
-
 def main():
-    """run the application"""
-
-    print('Hello World!')
-    
-    input("Press enter to exit ;)")
-    input()
-
+    print("Hello, World! Snapping along.")
 
 if __name__ == '__main__':
     main()
