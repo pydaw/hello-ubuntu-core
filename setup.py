0775766331397c9a074f6b03c485c70c9cb15c73
Daniel Wärnelöv <daniel.pean@gmail.com>
2023-06-21 21:40:44 +0200

    commit

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     url='https://github.com/yourusername/myprogram',
     author='Daniel Wärnelöv',
     author_email='daniel.pean@gmail.com',
-    py_modules=['helloworld'],
+    # py_modules=['helloworld'],
+    packages=['helloworld', 'bin']
+    include_package_data=True
     entry_points={
         'console_scripts': [
             'helloworld=helloworld:main',
