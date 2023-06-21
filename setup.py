2c414dd9370e8248e4585e298e2eb0168943df52
Daniel Wärnelöv <daniel.pean@gmail.com>
2023-06-21 20:59:12 +0200

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='hello-world-python',
+    name='helloworld',
     version='1.0.0',
     url='https://github.com/yourusername/myprogram',
-    author='Daniel Wärnelov',
+    author='Daniel Wärnelöv',
     author_email='daniel.pean@gmail.com',
     py_modules=['helloworld'],
     entry_points={
